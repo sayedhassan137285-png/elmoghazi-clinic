32d8f66e96fb101aaa3a8a466dfb849a60869d8e
sayedhassan137285-png <sayedhassan137285@gmail.com>
2026-09-18 01:09:46 +0300

    Create layout.tsx

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,9 @@
+import { AppShell, UserProvider } from "@/components/app-shell";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <UserProvider>
+      <AppShell>{children}</AppShell>
+    </UserProvider>
+  );
+}
